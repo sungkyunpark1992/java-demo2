21638fc820f90c10a9af87dc84ad69b633b9e32b
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-03 17:12:08 +0900

    java_jung_Ex6_11 : this와 this()의 차이점...+의문

--- src/Ex6_13.java
+++ src/Ex6_13.java
@@ -9,7 +9,7 @@ class Car2{
     Car2(String color){
         this(color, "auto", 4);// this(매개변수)는 생성자(메소드)
     }
-    Car2(String color, String gearType, int door){
+    Car2(String color, String gearType, int door){//얘는 그냥 메소드인가??
         this.color = color;// 인스턴스 자신을 가리키는 참조변수, 인스턴스의 주소가 저장되어 있다.
         this.gearType = gearType;
         this.door = door;
